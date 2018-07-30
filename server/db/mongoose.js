8eccce2c1aaef3b46b0a77bf2f8656ca64bf8d81
Yonatan <yona.mandel@gmail.com>
2018-07-30 12:56:25 +0300

    updated server to mLab server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@
   let mongoose = require('mongoose');
 
   mongoose.Promise = global.Promise;
-  mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds259111.mlab.com:59111/todo-app-api");
+  mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds259111.mlab.com:59111/todo-app-api" || "mongodb://127.0.0.1:27017/TodoApp");
 
   module.exports = {mongoose};
 
