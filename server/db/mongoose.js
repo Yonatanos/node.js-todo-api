e0151516f52ab710543f7582b3d431621201e458
Yonatan <yona.mandel@gmail.com>
2018-07-30 13:50:14 +0300

    Deployment test for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@
   let mongoose = require('mongoose');
 
   mongoose.Promise = global.Promise;
-  mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds259111.mlab.com:59111/todo-app-api" ||
-  "mongodb://127.0.0.1:27017/TodoApp");
+  mongoose.connect(//"mongodb://<dbuser>:<dbpassword>@ds259111.mlab.com:59111/todo-app-api" ||
+  process.env.PORT || "mongodb://127.0.0.1:27017/TodoApp");
 
   module.exports = {mongoose};
 
